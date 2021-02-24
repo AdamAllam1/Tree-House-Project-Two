556db5cb90b4af17c85f959295f15d1b3b6ba553
AdamAllam1 <AdamAllam1@users.noreply.github.com>
2021-02-24 00:36:18 -0500

    showPage Function work

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
 
 
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+
+
 
 
 
@@ -20,10 +13,36 @@ This function will create and insert/append the elements needed to display a "pa
 
 
 
+
+
+function showPage(list, page) {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const studentList = document.querySelector('.student-list')
+   studentList.innerHTML = '';
+   for (let i = 0; i < list.length; i++){
+      if (i >= startIndex && i < endIndex ) {
+         const studentItem = document.createElement('li');
+         
+      }
+   }
+}
+
+showPage(data, 3);
+
+
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+
+
+
+
+function addPagination() { 
+}
+
+
 
 
 
