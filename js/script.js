c0d57449ce3dc5d9370b0f64edab582db4dab6e2
AdamAllam1 <AdamAllam1@users.noreply.github.com>
2021-02-24 23:59:11 -0500

    D1 - Finished but doesn't work

--- js/script.js
+++ js/script.js
@@ -18,18 +18,24 @@ This function will create and insert/append the elements needed to display a "pa
 function showPage(list, page) {
    const startIndex = (page * 9) - 9;
    const endIndex = page * 9;
-   const studentList = document.querySelector('.student-list')
+   const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';
    for (let i = 0; i < list.length; i++){
       if (i >= startIndex && i < endIndex ) {
-         const studentItem = document.createElement('li');
-         
+         let studentItem = `<li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src='${data[i].picture.large}' alt="Profile Picture">
+           <h3>${data[i].name.first}</h3>
+           <span class="email">${data[i].email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">${data[i].registered.date}</span>
+         </div>
+       </li>`;
+       studentList.insertAdjacentHTML('beforeend', studentItem);
       }
    }
 }
-
-showPage(data, 3);
-
 
 /*
 Create the `addPagination` function
@@ -39,11 +45,27 @@ This function will create and insert/append the elements needed for the paginati
 
 
 
-function addPagination() { 
+function addPagination(list) { 
+   const numOfPages = Math.ceil(list.length / 9);
+   const linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
+   for (let i = 1; i <=numOfPages; i++) {
+      let button =  `<li><button type='button'>${i}</button></li>`
+      linkList.insertAdjacentHTML('beforeend', button);
+      document.querySelector('button').className = 'active';
+   }
+   linkList.addEventListener('click', (e) => {
+      if (e.tagName === 'BUTTON'){
+         document.querySelector('.active').className = '';
+         e.className = 'active';
+         showPage(list, e.textContent);
+      }
+   })
 }
 
 
 
-
-
 // Call functions
+
+showPage(data, 1);
+addPagination(data);
